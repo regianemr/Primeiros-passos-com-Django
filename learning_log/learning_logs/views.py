7f7be8366a13635aeaf2839eb4406150756ebe1c
Regiane <regianemelo1995@gmail.com>
2024-06-05 15:20:11 -0300

    template de topicos

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
 
 
 def index(request):
     '''A página inicial para o Registro de Aprendizagem'''
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Mostra todos os tópicos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
