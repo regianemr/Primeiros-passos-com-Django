58b202a9ab1c1bd57b057759330516d38eb67fb3
Regiane <regianemelo1995@gmail.com>
2024-06-05 11:37:58 -0300

    adicionando página inicial do exercicio 18.6 do livro

--- Pizzas/pizzas_log/views.py
+++ Pizzas/pizzas_log/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(render):
+def index(request):
     """A página inicial para o Registro de Aprendizagem"""
     return render(request, 'pizzas_log/index.html')
