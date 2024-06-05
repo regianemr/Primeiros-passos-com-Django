3f13959dff9567291565dd82e4bd889766b97bbc
Regiane <regianemelo1995@gmail.com>
2024-06-05 15:28:06 -0300

    view do topico

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Página que mostra todos os tópicos
     path('topics/', views.topics, name='topics'),
+    # Página de detalhes para um único tópico
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
