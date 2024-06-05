dcbec6c38baa8547eac1fb29becb802d86466316
Regiane <regianemelo1995@gmail.com>
2024-06-05 11:12:17 -0300

    Escrevendo uma view

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    '''A página inicial para o Registro de Aprendizagem'''
+    return render(request, 'learning_logs/index.html')
