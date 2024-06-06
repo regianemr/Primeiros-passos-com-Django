33dc1b72857d8ca990ef3ce9707dd4aa588d916b
Regiane <regianemelo1995@gmail.com>
2024-06-06 12:02:52 -0300

    exercicio 18.8 do livro

--- Pizzas/pizzas_log/urls.py
+++ Pizzas/pizzas_log/urls.py
@@ -8,4 +8,8 @@ app_name = 'pizzas_log'
 urlpatterns = [
     # Página inicial
     path('', views.index, name='index'),
+    # Página que mostra todos os ingredientes
+    path('pizzas/', views.pizzas, name='pizzas'),
+    # Página de detalhes para um único tópico
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
--- Pizzas/pizzas_log/views.py
+++ Pizzas/pizzas_log/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
+from .models import Pizza
 # Create your views here.
 
 
 def index(request):
     """A página inicial para o Registro de Aprendizagem"""
     return render(request, 'pizzas_log/index.html')
+
+
+def pizzas(request):
+    """Mostra todas as pizzas"""
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas_log/pizzas.html', context)
+
+
+def pizza(request, pizza_id):
+    """Mostra um único tópico"""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('-date_added')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas_log/pizza.html', context)
